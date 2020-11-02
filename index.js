288b2f1c2e516ac6c3e20dc7e39f32319862e2c6
Flipsterr <filip.von.bromssen@gmail.com>
2020-11-02 14:55:25 +0100

    first

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Read instructions. And remove this line.
+const message = document.getElementById("message")
+const numberInput = document.getElementById("user-input")
+const randomNumber = Math.round(Math.random()*100)
+
+function getUserGuess() {
+    return (parseInt(numberInput.value)) 
+}
+function setMessage(msg){
+    message.innerHTML = msg
+}
+
+document.addEventListener("keyup", function(event){
+    if (event.key === "Enter"){
+        if (randomNumber == getUserGuess()){
+            setMessage("You got it!")
+            return
+        } else if (randomNumber < getUserGuess()){
+            setMessage("Too high!")
+        }
+        else{
+            setMessage("Too low!")
+        }
+    }
+})
